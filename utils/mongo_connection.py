9aa8961e0b133e0b269743572445d97f21c2d14a
Daniel <katzz.daniel@gmail.com>
2024-04-22 01:38:55 +0300

    MongoDB class allows pulling data by three methods(should add a struct class with default format such as username, ip).

--- utils/mongo_connection.py
+++ utils/mongo_connection.py
@@ -1,24 +1,12 @@
 from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
 
-# Connection string with the root user credentials
-
-# Create a MongoClient to connect to MongoDB
-client = MongoClient(mongo_uri)
-
-# Access a database (or create it if it doesn't exist)
-db = client['test_database']
-
-# Access a collection and insert a document
-collection = db['example_collection']
-document = {"name": "John Doe", "age": 30}
-result = collection.insert_one(document)
-
-print("Document inserted with ID:", result.inserted_id)
 """
 This class MongoDB is used to access the mongoDB database.
 DataBase structure:
 users
-└─ cred -> username-ip-private_key(encrypted by given auth_key)
+└─ ip -> username-private_key(encrypted by given auth_key)
 message
 └─ ip -> encrypted_msg-send_to_username-timer_deletion
 """
@@ -27,5 +15,11 @@ class MongoDB:
         self.mongo_uri = "mongodb://admin:password@localhost:27017"
         self.client = MongoClient(self.mongo_uri)
 
-    def get_collection(self, ):
+    def get_collection_users(self, ip) -> Collection | None:
+        return self.client.get_database("users")[ip]
 
+    def get_collection_messages(self, ip) -> Collection | None:
+        return self.client.get_database("message")[ip]
+
+    def get_database(self, database_name) -> Database | None:
+        return self.client.get_database(database_name)
