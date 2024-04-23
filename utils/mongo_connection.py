61487e3f72923df32a6aa0c37d0ff7d15e7c3e1e
Daniel <katzz.daniel@gmail.com>
2024-04-23 22:50:19 +0300

    added insert functions and static build for User and Msg objects

--- utils/mongo_connection.py
+++ utils/mongo_connection.py
@@ -1,25 +1,33 @@
 from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
+from .pydantic_forms import *
+
+__all__ = ["MongoDB", "Msgs", "User"]
 
 """
 This class MongoDB is used to access the mongoDB database.
 DataBase structure:
 users
-└─ ip -> username-private_key(encrypted by given auth_key)
-message
-└─ ip -> encrypted_msg-send_to_username-timer_deletion
+└─ username -> ip-private_key(encrypted by given auth_key)
+msgs
+└─ username -> encrypted_msg-send_to_username-timer_deletion
 """
+
+
 class MongoDB:
     def __init__(self):
         self.mongo_uri = "mongodb://admin:password@localhost:27017"
         self.client = MongoClient(self.mongo_uri)
 
-    def get_collection_users(self, ip) -> Collection | None:
-        return self.client.get_database("users")[ip]
+    def insert_users(self, obj: User) -> None:
+        self.__insert_col(obj, self.client["users"])
 
-    def get_collection_messages(self, ip) -> Collection | None:
-        return self.client.get_database("message")[ip]
+    def insert_msg(self, obj: Msgs) -> None:
+        self.__insert_col(obj, self.client["msgs"])
 
-    def get_database(self, database_name) -> Database | None:
-        return self.client.get_database(database_name)
+    @staticmethod
+    def __insert_col(obj, db):
+        obj_modified = obj.dict()
+        obj_modified.pop("username")
+        db[obj.username].insert_one(obj_modified)
